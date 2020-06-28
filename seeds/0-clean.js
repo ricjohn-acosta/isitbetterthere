f0d3538ec28cbbe77898559cb3f4caff967c336d
ricjohn-acosta <ricjohn.genoguin@gmail.com>
2020-06-28 19:16:31 +1200

    change date_created table to string

--- seeds/0-clean.js
+++
@@ -1,8 +0,0 @@
-exports.seed = function (knex, Promise) {
-  const empty = (table) => () => knex(table).del();
-
-  return empty("experiences")();
-  // Chain calls to empty in
-  // order as required, e.g.:
-  // .then(empty('profiles'))
-};
