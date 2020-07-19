bccbaa795836067b60d64e452e08b19a0b9d853c
ricjohn-acosta <ricjohn.genoguin@gmail.com>
2020-07-19 23:08:04 +1200

    try testing account page with no data

--- pages/account.js
+++ pages/account.js
@@ -11,53 +11,53 @@ import {
 const account = (props) => {
   return (
     <Layout>
-      <Account {...props} />
+      {/* <Account {...props} /> */}
     </Layout>
   );
 };
 
-export async function getServerSideProps(context) {
-  const session = await getSession(context);
-  let user = null;
-  let userContributions = null;
-  let helpfulContributions = null;
+// export async function getServerSideProps(context) {
+//   const session = await getSession(context);
+//   let user = null;
+//   let userContributions = null;
+//   let helpfulContributions = null;
 
-  if (typeof window === "undefined" && context.res.writeHead) {
-    if (!session) {
-      context.res.writeHead(302, {
-        Location:
-          process.env.NODE_ENV === "production"
-            ? process.env.prod + "/signup"
-            : process.env.dev + "/signup",
-      });
-      context.res.end();
-    }
+//   if (typeof window === "undefined" && context.res.writeHead) {
+//     if (!session) {
+//       context.res.writeHead(302, {
+//         Location:
+//           process.env.NODE_ENV === "production"
+//             ? process.env.prod + "/signup"
+//             : process.env.dev + "/signup",
+//       });
+//       context.res.end();
+//     }
 
-    if (session) {
-      if (!(await getUser(session.id))) {
-        context.res.writeHead(302, {
-          Location:
-            process.env.NODE_ENV === "production"
-              ? process.env.prod + "/account-setup"
-              : process.env.dev + "/account-setup",
-        });
-        context.res.end();
-      } else {
-        user = await getUser(session.id);
-        userContributions = await getUserExperiences(session.id);
-        helpfulContributions = await getUserRatedExperiences(session.id);
-      }
-    }
-  }
+//     if (session) {
+//       if (!(await getUser(session.id))) {
+//         context.res.writeHead(302, {
+//           Location:
+//             process.env.NODE_ENV === "production"
+//               ? process.env.prod + "/account-setup"
+//               : process.env.dev + "/account-setup",
+//         });
+//         context.res.end();
+//       } else {
+//         user = await getUser(session.id);
+//         userContributions = await getUserExperiences(session.id);
+//         helpfulContributions = await getUserRatedExperiences(session.id);
+//       }
+//     }
+//   }
 
-  return {
-    props: {
-      session,
-      user,
-      userContributions,
-      helpfulContributions,
-    },
-  };
-}
+//   return {
+//     props: {
+//       session,
+//       user,
+//       userContributions,
+//       helpfulContributions,
+//     },
+//   };
+// }
 
 export default account;
