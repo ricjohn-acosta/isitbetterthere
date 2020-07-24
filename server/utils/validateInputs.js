30079487e13026467f1e86615738f9f92e485642
ricjohn-acosta <ricjohn.genoguin@gmail.com>
2020-07-25 02:40:52 +1200

    work on experiences validation

--- server/utils/validateInputs.js
+++ server/utils/validateInputs.js
@@ -1,12 +1,24 @@
+import validator from 'validator';
+
 export const isValid = (request, source) => {
-    
+
   // Grab the input values from user request
   switch (source) {
     case "new-experience":
-      const fields = Object.values(request).find(
-        (e) => e === false || e === ""
-      );
-      return fields ? false : true;
+      
+      if
+
+      if(validator.isEmpty(JSON.parse(request.story).blocks[0].text)) {
+        console.log("json")
+        return false
+      }
+
+      if(Object.values(request).find((e) => e === false || e === null)) {
+        console.log("request")
+        return false
+      }
+
+      return true;
 
     case "new-user":
       const { occupation, location, comes_from } = request;
