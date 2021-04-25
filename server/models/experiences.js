902853fb967d1cc8d021a2c96e2750b079c67d69
ricjohn-acosta <ricjohn.genoguin@gmail.com>
2021-04-26 01:15:27 +1200

    add missing import for careers list

---
+++ server/models/experiences.js
@@ -0,0 +1,62 @@
+import mongoose from 'mongoose'
+
+const ExperienceSchema = new mongoose.Schema({
+    posted_by: {
+        type: String,
+        required: [true, 'Please provide your name.'],
+        maxlength: [20, 'Name cannot be more than 20 characters'],
+    },
+    category: {
+        type: String,
+        required: [true, "Please provide a category."],
+        maxlength: [20],
+    },
+    from: {
+        type: String,
+        required: [true, 'Please specify where you are transitioning from'],
+        maxlength: [20],
+    },
+    to: {
+        type: String,
+        required: [true, 'Please specify where you are transitioning to.'],
+        maxlength: [20],
+    },
+    fulfillment: {
+        type: String,
+        required: [true, 'Please specify how fulfilling the transition was.'],
+        maxlength: [20],
+    },
+    ease_of_transition: {
+        type: String,
+        required: [true, 'Please specify how easy the transition was.'],
+        maxlength: [20],
+    },
+    regret: {
+        type: String,
+        required: [true, 'Please specify if you regret the transition.'],
+        maxlength: [20],
+    },
+    story: {
+        type: String,
+        required: [true, 'Please share your story.'],
+        maxlength: [10000]
+    },
+    helpful: {
+        type: Number,
+        required: true,
+    },
+    not_helpful: {
+        type: Number,
+        required: true,
+    },
+    users_helped: {
+        type: Array,
+        required: true,
+    },
+    date_posted: {
+        type: Number,
+        required: [true, 'Please specify the species of your pet.'],
+    },
+})
+
+const experiencesCollection = mongoose.models.Experiences || mongoose.model('Experience', ExperienceSchema)
